78c1bc1e5f415223bc2028edc603a21b9615205d
Lhwhatever <llinhong.2301+github@gmail.com>
2020-06-17 14:35:22 +0800

    Setup prettier for TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,29 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2020': true,
-        'node': true
+    env: {
+        browser: true,
+        es2020: true,
+        node: true
     },
-    'extends': [
+    extends: [
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
         },
-        'ecmaVersion': 11,
-        'sourceType': 'module'
+        ecmaVersion: 11,
+        sourceType: 'module'
     },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never']
     }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
 
 function IndexPage(): React.ReactElement {
-    return (
-        <div>Hello</div>
-    )
+    return <div>Hello</div>
 }
 
 export default IndexPage
