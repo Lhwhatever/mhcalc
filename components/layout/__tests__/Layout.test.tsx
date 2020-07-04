355366671c94f351055302bdbf9a67da1e183d12
Lhwhatever <llinhong.2301+github@gmail.com>
2020-07-04 22:49:57 +0800

    Add onOpen/onClose integration tests for NavDrawer in Layout

--- components/layout/__tests__/Layout.test.tsx
+++ components/layout/__tests__/Layout.test.tsx
@@ -5,6 +5,7 @@ import theme from '../../../theme'
 import Layout from '../Layout'
 import Navbar from '../Navbar'
 import NavDrawer from '../NavDrawer'
+import { act } from 'react-dom/test-utils'
 
 jest.mock('next/router', () => ({
     useRouter() {
@@ -58,7 +59,21 @@ describe('Layout test', () => {
 
     it('should open the drawer when clicked', () => {
         expect(wrapper.find(NavDrawer).prop('open')).toBeFalsy()
-        wrapper.find(Navbar).find(IconButton).simulate('click')
+        wrapper.find(Navbar).find(IconButton).invoke('onClick')()
         expect(wrapper.find(NavDrawer).prop('open')).toBeTruthy()
+    })
+
+    it('should not open/close when a Tab or Shift keydown event is given', () => {
+        expect(wrapper.find(NavDrawer).prop('open')).toBeFalsy()
+
+        wrapper.find(NavDrawer).invoke('onOpen')({ type: 'keydown', key: 'Tab' })
+        expect(wrapper.find(NavDrawer).prop('open')).toBeFalsy()
+        wrapper.find(NavDrawer).invoke('onOpen')({ type: 'keydown', key: 'Shift' })
+        expect(wrapper.find(NavDrawer).prop('open')).toBeFalsy()
+
+        wrapper.find(NavDrawer).invoke('onClose')({ type: 'keydown', key: 'Tab' })
+        expect(wrapper.find(NavDrawer).prop('open')).toBeFalsy()
+        wrapper.find(NavDrawer).invoke('onClose')({ type: 'keydown', key: 'Shift' })
+        expect(wrapper.find(NavDrawer).prop('open')).toBeFalsy()
     })
 })
