7521f576aee79cdf9beb47b92342e16cbf28de53
Lhwhatever <llinhong.2301+github@gmail.com>
2020-07-07 22:47:01 +0800

    Fix incorrect test expectation in utils/number

--- utils/__tests__/number.test.ts
+++ utils/__tests__/number.test.ts
@@ -6,7 +6,7 @@ describe('number test', () => {
         expect(coerceToRange(-1, undefined, undefined)).toBe(-1)
 
         expect(coerceToRange(5, 1, undefined)).toBe(5)
-        expect(coerceToRange(1, undefined, 10)).toBe(5)
+        expect(coerceToRange(1, undefined, 10)).toBe(1)
 
         expect(coerceToRange(0, 1, undefined)).toBe(1)
         expect(coerceToRange(5, undefined, 4)).toBe(4)
