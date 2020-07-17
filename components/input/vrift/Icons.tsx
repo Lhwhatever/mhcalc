017917e1de95e75867deead1d9c57fd5709ca223
Lhwhatever <llinhong.2301+github@gmail.com>
2020-07-17 23:19:42 +0800

    Add ChampFireIcon

--- components/input/vrift/Icons.tsx
+++ components/input/vrift/Icons.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, withStyles } from '@material-ui/core'
 import React from 'react'
 
 export const SyncIcon = (): JSX.Element => (
@@ -23,4 +23,35 @@ export const SiphonIcon = (): JSX.Element => (
         src="https://www.mousehuntgame.com/images/ui/hud/rift_valour/power_up_siphon.png"
         variant="rounded"
     />
+)
+
+export interface ChampFireIconProps {
+    state?: boolean
+}
+
+const champFireStyles = {
+    default: {
+        width: '100%',
+        paddingBottom: '100%',
+        backgroundImage: 'url(https://www.mousehuntgame.com/images/ui/hud/rift_valour/fuel.png)',
+        backgroundPosition: '0 0',
+        backgroundSize: '100%',
+        borderRadius: '50%'
+    },
+    active: {
+        backgroundPosition: '0% 100%'
+    }
+}
+
+export const ChampFireIcon = withStyles(champFireStyles)(
+    ({ state = false }: ChampFireIconProps): JSX.Element => (
+        <div
+            style={{
+                ...champFireStyles.default,
+                ...(state ? champFireStyles.active : undefined)
+            }}
+            role="img"
+            aria-label="champion's fire"
+        />
+    )
 )
