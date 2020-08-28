644f07209cddca3efab5423e7a879ee4cb93e60e
Lhwhatever <llinhong.2301+github@gmail.com>
2020-08-28 22:04:20 +0800

    First steps in implementation of SetupInputGroup
    Preliminary implementation of speed inputs. Currently, you can edit the
    speeds of the given floor entries, and delete those entries where floor
    > 1.

--- components/input/vrift/SetupInputGroup.tsx
+++ components/input/vrift/SetupInputGroup.tsx
@@ -1,12 +1,133 @@
+import { Box, BoxProps, Paper, Tab, Tabs, Typography, makeStyles } from '@material-ui/core'
 import React from 'react'
-import { Box, Typography, BoxProps } from '@material-ui/core'
+import { PowerIcon } from '../../icons/TrapStatIcons'
+import { SpeedIcon, SiphonIcon } from './Icons'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../../../redux/rootReducer'
+import SpeedInput from './SpeedInput'
+import { updateSpeed, deleteSpeed } from '../../../redux/ducks/vrift/setups'
+import { Speed } from '../../../redux/ducks/vrift/stats'
+import { formatRange } from '../../../utils/range'
+import createMenu, { MenuItem } from '../../menus/createMenu'
+
+const useStyles = makeStyles((theme) => ({
+    inputContainer: {
+        margin: theme.spacing(1),
+        display: 'grid',
+        gridTemplateColumns: 'auto 1fr auto',
+        gridGap: theme.spacing(1)
+    },
+    statIcon: {
+        gridColumnStart: 1,
+        gridColumnEnd: 'span 1'
+    },
+    statInput: {
+        gridColumnStart: 2,
+        gridColumnEnd: 'span 1'
+    }
+}))
+
+const createA11yPropsFactory = (prefix: string) => ({
+    createTabProps: (index: number) => ({
+        id: `${prefix}-tab-${index}`,
+        'aria-controls': `${prefix}-tabpanel-${index}`
+    }),
+    createTabPanelProps: (index: number, selectedIndex: number) => ({
+        role: 'tabpanel',
+        hidden: index !== selectedIndex,
+        id: `${prefix}-tabpanel-${index}`,
+        'aria-labelledby': `${prefix}-tab-${index}`
+    })
+})
+
+export interface CategoryGroupProps<P> {
+    component: React.ElementType<React.HTMLAttributes<HTMLElement>>
+    componentProps?: P
+    children?: React.ReactNode
+}
+
+export const CategoryGroup = <P,>(props: CategoryGroupProps<P>): JSX.Element => {
+    const { component: Component, componentProps, children } = props
+    return <Component {...componentProps}>{children}</Component>
+}
 
 type SetupInputGroupProps = Pick<BoxProps, 'className'>
 
 const SetupInputGroup = (props: SetupInputGroupProps): JSX.Element => {
+    const classes = useStyles()
+
+    const [category, setCategory] = React.useState(0)
+
+    const handleCategoryChange = (_: React.ChangeEvent<{}>, newValue: number) => {
+        setCategory(newValue)
+    }
+
+    const { createTabProps, createTabPanelProps } = createA11yPropsFactory('setup-input')
+
+    const { speed, siphon } = useSelector((state: RootState) => state.vrift.setups)
+
+    const dispatch = useDispatch()
+
+    const createSpeedInputs = (): JSX.Element[] => {
+        const floors = Object.keys(speed).map((x) => parseInt(x, 10))
+        floors.sort()
+
+        const floorRanges = formatRange(floors)
+
+        return floors.map((floor, index) => {
+            const handleSpeedChange = () => 1
+
+            const menuItems: MenuItem[] = [{ item: 'Edit' }, { item: 'Duplicate' }]
+            if (floor > 1) menuItems.push({ item: 'Delete', onClick: () => dispatch(deleteSpeed(floor)) })
+
+            const Menu = createMenu(menuItems, {
+                menuId: `speed-options-f${floor}`,
+                ariaLabel: `more options for speed (floors ${floorRanges[index]})`
+            })
+
+            return (
+                <React.Fragment key={floor}>
+                    <div className={classes.statInput}>
+                        <SpeedInput
+                            id={`speed-input-f${floor}`}
+                            label={`Speed (Floors ${floorRanges[index]})`}
+                            value={speed[floor]}
+                            onChange={handleSpeedChange}
+                        />
+                    </div>
+                    <div>
+                        <Menu />
+                    </div>
+                </React.Fragment>
+            )
+        })
+    }
+
     return (
         <Box {...props}>
             <Typography variant="h4">Setup</Typography>
+            <Box className={classes.inputContainer}>
+                <div className={classes.statIcon}>
+                    <SpeedIcon />
+                </div>
+                {createSpeedInputs()}
+                <div className={classes.statIcon}>
+                    <SiphonIcon />
+                </div>
+            </Box>
+            <Paper square>
+                <Tabs value={category} onChange={handleCategoryChange} aria-label="setup category" variant="fullWidth">
+                    <Tab label="Eclipse" {...createTabProps(0)} />
+                    <Tab label="Non-Eclipse" {...createTabProps(1)} />
+                </Tabs>
+            </Paper>
+            <Box className={classes.inputContainer}>
+                <CategoryGroup component="div" componentProps={createTabPanelProps(1, category)}>
+                    <div className={classes.statIcon}>
+                        <PowerIcon />
+                    </div>
+                </CategoryGroup>
+            </Box>
         </Box>
     )
 }
--- components/input/vrift/__tests__/SetupInputGroup.test.tsx
+++ components/input/vrift/__tests__/SetupInputGroup.test.tsx
@@ -1,13 +1,150 @@
+import userEvent from '@testing-library/user-event'
 import React from 'react'
-import { render, screen } from '../../../../utils/testing/test'
+import mockStore from '../../../../utils/testing/mockStore'
+import { createRenderWithRedux, screen, getRoles, prettyDOM, cleanup, within } from '../../../../utils/testing/test'
 import SetupInputGroup from '../SetupInputGroup'
+import { AnyAction } from 'redux'
+import { deleteSpeed } from '../../../../redux/ducks/vrift/setups'
+import { MockStoreEnhanced } from 'redux-mock-store'
 
 describe('SetupInputGroup test', () => {
+    const categoryMatchers = {
+        eclipse: /^Eclipse$/i,
+        regular: /^Non-Eclipse$/i
+    }
+
+    const getRegularTab = () => screen.getByRole('tab', { name: categoryMatchers.regular })
+
+    const store = mockStore({
+        vrift: {
+            setups: {
+                speed: { [1]: 10 },
+                siphon: { [1]: 25 }
+            }
+        }
+    })
+
+    const altStore = mockStore({
+        vrift: {
+            setups: {
+                speed: { [1]: 7, [17]: 8 },
+                siphon: { [1]: 20, [25]: 25 }
+            }
+        }
+    })
+
+    const setupStoreActions = (store: MockStoreEnhanced<unknown, {}>): unknown[] => {
+        store.clearActions()
+        return store.getActions()
+    }
+
+    const getOptionsBtn = (stat: string, floorRange: string) =>
+        screen.getByRole('button', {
+            name: new RegExp(`more options for ${stat} \\(floors ${floorRange.replace(/\+/g, '\\+')}\\)`, 'i')
+        })
+
+    const renderTarget = <SetupInputGroup />
+
     beforeEach(() => {
-        render(<SetupInputGroup />)
+        createRenderWithRedux(store)(renderTarget)
     })
 
     it('should have a heading', () => {
         expect(screen.getByRole('heading', { name: /Setup/i })).toBeInTheDocument()
+    })
+
+    it('should have a tab selector', () => {
+        expect(screen.getByRole('tablist', { name: /Setup Category/i })).toBeInTheDocument()
+        expect(screen.getByRole('tab', { name: categoryMatchers.eclipse })).toBeInTheDocument()
+        expect(getRegularTab()).toBeInTheDocument()
+    })
+
+    it('should have one icon for each shared stat', () => {
+        expect(screen.getByRole('img', { name: /Speed/i })).toBeInTheDocument()
+        expect(screen.getByRole('img', { name: /Siphon/i })).toBeInTheDocument()
+    })
+
+    it('should have one speed input per speed entry', () => {
+        expect(screen.getByLabelText(/(?<!options.*)speed \(floors 1\+\)/i)).toBeInTheDocument()
+
+        cleanup()
+        createRenderWithRedux(altStore)(renderTarget)
+        expect(screen.getByLabelText(/(?<!options.*)speed \(floors 1-16\)/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/(?<!options.*)speed \(floors 17\+\)/i)).toBeInTheDocument()
+    })
+
+    describe('test speed options button', () => {
+        const getMenu = () => screen.getByRole('menu')
+
+        function testForEditBtn(menu: HTMLElement, floor: number, actions: unknown[]) {
+            const editBtn = within(menu).getByRole('menuitem', { name: /edit/i })
+            expect(editBtn).toBeInTheDocument()
+        }
+
+        function testForDuplicateBtn(menu: HTMLElement, floor: number, actions: unknown[]) {
+            const dupeBtn = within(menu).getByRole('menuitem', { name: /duplicate/i })
+            expect(dupeBtn).toBeInTheDocument()
+        }
+
+        function testForDeleteBtn(menu: HTMLElement, floor: number, actions: unknown[]) {
+            const deleteBtn = within(menu).getByRole('menuitem', { name: /delete/i })
+
+            expect(deleteBtn).toBeInTheDocument()
+            userEvent.click(deleteBtn)
+
+            expect(actions).toContainEqual(deleteSpeed(floor))
+        }
+
+        beforeEach(() => {
+            cleanup()
+        })
+
+        it('should have one options menu when there is one speed entry', () => {
+            createRenderWithRedux(store)(renderTarget)
+
+            const optionsBtn = getOptionsBtn('speed', '1+')
+
+            userEvent.click(optionsBtn)
+            testForEditBtn(getMenu(), 1, setupStoreActions(store))
+
+            userEvent.click(optionsBtn)
+            testForDuplicateBtn(getMenu(), 1, setupStoreActions(store))
+        })
+
+        it('should have two options when there are two speed entries', () => {
+            createRenderWithRedux(altStore)(renderTarget)
+
+            const optionsBtn1 = getOptionsBtn('speed', '1-16')
+            const optionsBtn17 = getOptionsBtn('speed', '17+')
+
+            userEvent.click(optionsBtn1)
+            testForEditBtn(getMenu(), 1, setupStoreActions(altStore))
+
+            userEvent.click(optionsBtn1)
+            testForDuplicateBtn(getMenu(), 1, setupStoreActions(altStore))
+
+            userEvent.click(optionsBtn17)
+            testForEditBtn(getMenu(), 17, setupStoreActions(altStore))
+
+            userEvent.click(optionsBtn17)
+            testForDuplicateBtn(getMenu(), 17, setupStoreActions(altStore))
+
+            userEvent.click(optionsBtn17)
+            testForDeleteBtn(getMenu(), 17, setupStoreActions(altStore))
+        })
+    })
+
+    describe('Non-Eclipse setups', () => {
+        beforeEach(() => {
+            userEvent.click(getRegularTab())
+        })
+
+        it('should have a tabpanel for non-eclipse', () => {
+            expect(screen.getByRole('tabpanel', { name: categoryMatchers.regular })).toBeInTheDocument()
+        })
+
+        it('should have one icon for each stat', () => {
+            expect(screen.getByRole('img', { name: /Power/i })).toBeInTheDocument()
+        })
     })
 })
