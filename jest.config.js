f21475464f0a37ba86c33862675e5de9c9b4a308
Lhwhatever <llinhong.2301+github@gmail.com>
2020-08-28 21:51:23 +0800

    Swap out ts-jest for babel-jest
    The typechecking functions of ts-jest will now be achieved using
    tsc --noEmit.
    
    This is enforced in the hooks relating to the preview and master
    branches.

--- jest.config.js
+++ jest.config.js
@@ -58,12 +58,7 @@ module.exports = {
     // globalTeardown: undefined,
 
     // A set of global variables that need to be available in all test environments
-    globals: {
-        'ts-jest': {
-            tsConfig: '<rootDir>/config/tsconfig.jest.json',
-            babelConfig: true
-        }
-    },
+    globals: {},
 
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
@@ -89,7 +84,7 @@ module.exports = {
     // notifyMode: "failure-change",
 
     // A preset that is used as a base for Jest's configuration
-    preset: 'ts-jest',
+    // preset: '',
 
     // Run tests from one or more projects
     // projects: undefined,
@@ -164,7 +159,7 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     transform: {
-        '^.+\\.tsx?$': 'ts-jest'
+        '^.+\\.tsx?$': 'babel-jest'
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
